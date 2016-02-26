924f910d51308e7b1df2644c4ffb12304b86f76f
Mike Moore <tigerhollowpoint@gmail.com>
2016-02-26 11:24:32 -0500

    working on a preloader

---
+++ template-preloader.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: PRELOADER
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>App Preloader</title>
+    <script src="https://code.createjs.com/preloadjs-0.6.2.min.js"></script>
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?v=3&amp;libraries=geometry"></script>
+
+  </head>
+  <body>
+
+  </body>
+</html>
